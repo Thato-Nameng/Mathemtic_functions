ecc83538abfa875f1aaf08661ad9fa509c8c32f2
Thato Omphile Nameng <71035775+Thato-Nameng@users.noreply.github.com>
2022-02-13 14:23:22 +0200

    Add files via upload

---
+++ Mathematical Functions/Mathematical Functions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mathematical_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double angleDegres, inRadians, inDegrees, positive;
+            Console.Write("Enter an angle in degrees: ");
+            angleDegres = Convert.ToDouble(Console.ReadLine());
+
+              inRadians = (angleDegres * (Math.PI) / 180);
+            inDegrees = Math.Sin(inRadians);
+            Console.WriteLine("Sine of the angle is: " + Math.Round(inDegrees, 2));
+
+            Console.ReadLine();
+            Console.Write("Enter a positive number: ");
+            positive = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Square root of the number is: " + Math.Sqrt(positive));
+            Console.ReadLine();
+        }
+    }
+}
